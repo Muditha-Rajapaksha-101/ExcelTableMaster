2b46f756c0174a66e395544d25a296874bd86f41
Muditha Rajapaksha <119531200+Muditha-Rajapaksha-101@users.noreply.github.com>
2022-12-01 14:52:37 +0530

    Update example.py

--- example.py
+++ example.py
@@ -1,7 +1,10 @@
 from ExcelTableMaster.ExcelTableMaster import ExcelTableMaster
 
 if __name__ == "__main__":    
-    tableMaster = ExcelTableMaster( fileName = "my.xlsx", workSheetIndex = 0, tableName = "data")
-    #tableMaster.writeTableRow( dataList = [2 , "nimae" , 12])
+    tableMaster = ExcelTableMaster( fileName = "data.xlsx", workSheetIndex = 0, tableName = "data")
+    tableMaster.writeTableRow( dataList = [3 , "Dave" , 12])
+    tableMaster.writeMultipleTableRow( dataList = [[2 , "Sunny" , 12] , [3 , "Nimal" , 35]])
     tableMaster.writeUniqueTableRow( keyColumnLetter =  "A" , keyIndexNo = 0, dataList = [66 , "HHH" , 12])
-    #print(tableMaster.ValueExist("9" , "A"))
+    tableMaster.writeMultipleUniqueTableRow( keyColumnLetter =  "A" , keyIndexNo = 0, dataList = [[2 , "Sunny" , 12] , [3 , "Nimal" , 35]])
+    
+    print(tableMaster.ValueExist("9" , "A"))
